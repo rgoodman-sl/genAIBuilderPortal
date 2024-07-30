9cd4ea6ea1d3fb49bf49ec9e674996760db28864
rgoodman-sl <68026685+rgoodman-sl@users.noreply.github.com>
2024-07-30 03:43:00 +0000

    Manufacturing Specs Changes

--- pages/7_ 📐_German Manufacturing Spec Assistant.py
+++ pages/7_ 📐_German Manufacturing Spec Assistant.py
@@ -10,41 +10,59 @@ def typewriter(text: str, speed: int):
         container.markdown(curr_full_text)
         time.sleep(1 / speed)
 
+if 'doc_key' not in st.session_state:
+    st.session_state.doc_key = None
+    st.session_state.doc_status = 0
+
+def doc_proc():
+    if st.session_state.doc_key == None:
+        st.session_state.doc_status = 0
+    if st.session_state.doc_key != None:
+        st.session_state.doc_status = 1
+
+
 st.set_page_config(page_title="GenAI Builder - Chatbot")
 st.title("Manufacturing Spec Assistant")
 
-st.markdown(
-    """  
-    ### This is an Assistant to help users retrieve information from Manufacturing Specifications and Tests.
-    Examples: 
-    - What are the university values for advertising
-    - What is the Code of Conduct?
-    - Describe the Community Safety Expectations for Students
-    - What are the university provisions regarding alcohol?
-    """)
+with st.container(height=300):
+    st.markdown(
+        """  
+        #### This is an Assistant to help users retrieve information from Manufacturing Specifications and Tests. ####
+        Examples: 
+        | Document | Question|
+        | -------- | :-------- |
+        | MSG_01_HGPL-B.pdf | What is the weight of the grippers by size? |
+        | MSG_01_HGPL-B.pdf | What is the maximum operating frequency of the gripper? |
+        | MSG_02_HGPT_HGPL_HGDT.pdf | What are the differences between the various grippers? |
+        | MSG_03_measurement_test_json.json | In which interval is the setpoint for the density? |
+        | MSG_04_measurement_test.pdf | In which interval is the setpoint for the density? |
+        | MSG_04_measurement_test.pdf | In which interval is the actual value for the density? |
+        """)
+
 
 # Document Selection
-genre = st.radio(
-    "What's your favorite movie genre",
-    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+doc = st.radio(
+    "**Which Document are you inquiring about?**",
+    ["MSG_01_HGPL-B.pdf", "MSG_02_HGPT_HGPL_HGDT.pdf", "MSG_04_measurement_test.pdf"],
     index=None,
+    key='doc_key',
+    on_change=doc_proc(),
+    
 )
 
-st.write("You selected:", genre)
+#st.write("You selected:", doc)
+
+# Create Input Message
+if st.session_state.doc_status == 0:
+    st.inputmsg = "Choose a Document to begin chat"
+    st.chatDisabled = True
+if st.session_state.doc_status == 1:
+    st.inputmsg = "What do you want to know about "+st.session_state.doc_key
+    st.chatDisabled = False
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
-# Allow user to select Document
-option = st.selectbox(
-    "Which Document are you inquiring about?",
-    ("MSG_01_HGPL-B.pdf", "MSG_02_HGPT_HGPL_HGDT.pdf", "MSG_04_measurement_test.pdf"),
-    index=None,
-    placeholder="Select contact Document...",
-    
-st.write("You selected:", option)
-)
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -52,21 +70,21 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # React to user input
-prompt = st.chat_input("Ask me anything related to Fordham University Regulations")
+prompt = st.chat_input(placeholder=st.inputmsg,disabled=st.chatDisabled)
 if prompt:
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    URL = 'https://abd36121231c84034b34129e798681ad-485346289.eu-west-3.elb.amazonaws.com/api/1/rest/feed-master/queue/ConnectFasterInc/RG/msg/MSG_Retriever%20Task'
-    BEARER_TOKEN = 'FXxWoR4LqJOGLJ6OZJeoC8xxHAvRyk7Y'
+    URL = 'https://abd36121231c84034b34129e798681ad-485346289.eu-west-3.elb.amazonaws.com/api/1/rest/feed-master/queue/ConnectFasterInc/RG/msg/MSG_Ultra%20Task'
+    BEARER_TOKEN = 's7R0GSJF7AfAayGFrSxHeFC3Tc1tjo3P'
 
-    data = {"prompt" : prompt}
+    data = {"prompt" : prompt, "Document" : st.session_state.doc_key}
 
     headers = {
         'Authorization': f'Bearer {BEARER_TOKEN}'
     }
-
+    
     response = requests.post(
         url=URL,
         data=data,
@@ -77,11 +95,12 @@ if prompt:
 
     result = response.json()
     #st.write(result)
-    response=result[0]['choices'][0]['message']['content']
+    response=result['choices'][0]['message']['content']
+
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        #st.markdown(response,unsafe_allow_html=True)
-        typewriter(text=response, speed=10)
+        st.markdown(response,unsafe_allow_html=True)
+        #typewriter(text=response, speed=10)
 
 # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
