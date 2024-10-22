4952751cd465e7b09a9993c13899a9da45e82b71
rgoodman-sl <68026685+rgoodman-sl@users.noreply.github.com>
2024-10-22 05:49:58 +0000

    Focus Lock?

--- GenAI_Demo.py
+++ GenAI_Demo.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from dotenv import dotenv_values
+import sys
 
 
 # Load environment
@@ -9,10 +10,44 @@ page_title=env["PAGE_TITLE"]
 title=env["TITLE"]
 
 
+def clear_chat_history(page_name):
+    """
+    Clear chat history for a specific page
+    """
+    chat_histories = {
+        'E-Commerce DB Agent': 'EC_messages',
+        'E-Commerce DB Agent_Expandable Log': 'ECE_messages',
+        'HR Chatbot': 'HR_messages',
+        'University Policy Agent': 'UNI_messages',
+        'Deloitte Chatbot': 'DL_messages',
+        'Content Reconciliation': 'CREC_messages',
+        'Competitor Product Agent': 'CPA_messages',
+        'Health Insurance Chatbot': 'UW_messages'
+    }
+    
+    # If the page has a chat history, clear it
+    if page_name in chat_histories:
+        history_key = chat_histories[page_name]
+        if history_key in st.session_state:
+            del st.session_state[history_key]
+
+
+# Get the name of the current page
+try:
+    current_page = sys.argv[4].split('/')[-1].replace('.py', '')
+except:
+    current_page = 'GenAI_Demo'
+
+# If we're navigating to a new page, clear the previous page's history
+if 'previous_page' in st.session_state:
+    if st.session_state.previous_page != current_page:
+        clear_chat_history(st.session_state.previous_page)
+
+# Store the current page for next time
+st.session_state.previous_page = current_page
+
 st.set_page_config(page_title=page_title)
 st.title(title)
-
-
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
@@ -27,3 +62,10 @@ st.markdown(
     - Ask a question in our [community](https://community.snaplogic.com)
 """
 )
+
+# Add a manual clear button to the sidebar for the current page
+if current_page != 'GenAI_Demo':
+    if st.sidebar.button('Clear Current Chat History'):
+        clear_chat_history(current_page)
+        st.sidebar.success(f"Chat history cleared for {current_page}")
+        st.experimental_rerun()
